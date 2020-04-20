579c554065e8736a4ec6fc51361e25e72cd6c626
J-F <jffontaine_@hotmail.com>
2020-04-20 19:46:36 -0400

    return fait pour les states.

--- utils.py
+++ utils.py
@@ -20,6 +20,7 @@ def return_state(direction_actuelle, position_bouffe, x, y, positions_valides):
     bouffe_est_a_gauche = 0
     bouffe_est_a_droite = 0
     bouffe_devant = 0
+    bouffe_derriere = 0 
     libre_a_gauche = 0
     libre_a_droite = 0
     libre_en_avant = 0
@@ -33,10 +34,13 @@ def return_state(direction_actuelle, position_bouffe, x, y, positions_valides):
         bouffe_est_a_droite = int(position_bouffe[1] > y)
         bouffe_devant = int(position_bouffe[1] > x)
         bouffe_derriere = int(position_bouffe[1] < x)
-
         libre_a_gauche = int(not positions_valides.estDansGrille((x, y - step)))
         libre_a_droite = int(not positions_valides.estDansGrille((x, y + step)))
         libre_en_avant = int(not positions_valides.estDansGrille((x + step, y)))
+        haut = 0
+        bas = 0
+        droite = 1
+        gauche = 0
 
     if direction_actuelle == "GAUCHE":
         bouffe_est_a_gauche = int(position_bouffe[1] > y)
@@ -46,6 +50,10 @@ def return_state(direction_actuelle, position_bouffe, x, y, positions_valides):
         libre_a_gauche = int(not positions_valides.estDansGrille((x, y + step)))
         libre_a_droite = int(not positions_valides.estDansGrille((x, y - step)))
         libre_en_avant = int(not positions_valides.estDansGrille((x - step, y)))
+        haut = 0
+        bas = 0
+        droite = 0
+        gauche = 1
 
     if direction_actuelle == "HAUT":
         bouffe_est_a_gauche = int(position_bouffe[0] < x)
@@ -55,6 +63,10 @@ def return_state(direction_actuelle, position_bouffe, x, y, positions_valides):
         libre_a_gauche = int(not positions_valides.estDansGrille((x - step, y)))
         libre_a_droite = int(not positions_valides.estDansGrille((x + step, y)))
         libre_en_avant = int(not positions_valides.estDansGrille((x, y - step)))
+        haut = 1
+        bas = 0
+        droite = 0
+        gauche = 0
 
     if direction_actuelle == "BAS":
         bouffe_est_a_gauche = int(position_bouffe[0] > x)
@@ -64,4 +76,10 @@ def return_state(direction_actuelle, position_bouffe, x, y, positions_valides):
         libre_a_gauche = int(not positions_valides.estDansGrille((x + step, y)))
         libre_a_droite = int(not positions_valides.estDansGrille((x - step, y)))
         libre_en_avant = int(not positions_valides.estDansGrille((x, y + step)))
+        haut = 0
+        bas = 1
+        droite = 0
+        gauche = 0
 
+    return np.array([bouffe_est_a_gauche, bouffe_est_a_droite, bouffe_devant, bouffe_derriere, libre_a_gauche,
+                     libre_a_droite, libre_en_avant, haut, bas, droite, gauche])
